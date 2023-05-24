666b71ceab89ff9d74167ef24182ce3a813080b3
Akram Boktor <akram.boktor2013@gmail.com>
2023-05-24 19:44:22 +0200

    inject Product Repository in the controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -10,17 +11,23 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
+        private readonly IProductRepository _productRepository;
 
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository productRepository)
         {
-            _context = context;
+            _productRepository = productRepository;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Products>>> GetProducts()
         {
-            return Ok(await _context.Products.ToListAsync());
-        }  
+            return Ok(await _productRepository.GetProductsAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Products>> GetProduct(int id)
+        {
+            return Ok(await _productRepository.GetProductByIdAsync(id));
+        }
     }
 }
