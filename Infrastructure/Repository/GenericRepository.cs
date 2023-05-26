b52a2dd6bc73df6214573b53f3909f91b2fd8412
Akram Boktor <akram.boktor2013@gmail.com>
2023-05-26 15:56:08 +0200

    Add Generic Repository and IGeneric Repository in solution

---
+++ Infrastructure/Repository/GenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> LIstAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
