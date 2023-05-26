e8261c6e9e1dd11cd72239f93c9ee2bdad06f878
Akram Boktor <akram.boktor2013@gmail.com>
2023-05-26 08:11:07 +0200

    add two method for product type and product brand in product controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -19,7 +19,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Products>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<Products>>> GetProducts()
         {
             return Ok(await _productRepository.GetProductsAsync());
         }
@@ -28,6 +28,18 @@ namespace API.Controllers
         public async Task<ActionResult<Products>> GetProduct(int id)
         {
             return Ok(await _productRepository.GetProductByIdAsync(id));
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _productRepository.GetProductBrandAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductTypes()
+        {
+            return Ok(await _productRepository.GetProductTypeAsync());
         }
     }
 }
