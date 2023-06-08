9b28653ad1e8c7ed8352eac05e645cf62e62eacf
Akram Boktor <akram.boktor2013@gmail.com>
2023-06-08 18:46:13 +0200

    Add Api response class

--- API/Errors/ApiResponse.cs
+++ API/Errors/ApiResponse.cs
@@ -2,7 +2,26 @@ namespace API.Errors
 {
     public class ApiResponse
     {
-        public int StatusCode { get; set; }
+        public ApiResponse(int statusCode , string Message =  null)
+        {
+            this.statusCode = statusCode;
+            this.Message = Message ?? GetDefaultMessgeForStatusCode(statusCode);
+        }
+
+        public int statusCode { get; set; }
         public string Message { get; set; }
+
+        private string GetDefaultMessgeForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+
+                400 => "A bad request , you have made",
+                401 => "Un Authorized",
+                404 => "Resource not found",
+                500 => "Bad request internal server error",
+                _ => null
+            };
+        }
     }
 }
