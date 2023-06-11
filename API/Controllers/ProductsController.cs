3902795e11a5f7162a6e22feed553f6a74314833
Akram Boktor <akram.boktor2013@gmail.com>
2023-06-11 11:05:37 +0200

    improve swagger documnet

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -41,6 +41,8 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
