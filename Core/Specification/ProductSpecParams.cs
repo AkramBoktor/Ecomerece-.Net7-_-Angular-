8c2d3b3863d9fb932e5d122f58a1b06c00f91b52
Akram Boktor <akram.boktor2013@gmail.com>
2023-06-15 19:30:45 +0200

    Add product spec parameter

---
+++ Core/Specification/ProductSpecParams.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specification
+{
+    public class ProductSpecParams
+    {
+        private const int MaxPageSize = 40;
+        public int PageIndex { get; set; } = 1;
+
+        private int _PageSize { get; set; } = 6;
+
+        public int PageSize {
+            get => _PageSize;
+            set => _PageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+
+        public int? BrandId { get; set; }
+
+        public int? TypeId { get; set; }
+
+        public string sort { get; set; }
+    }
+}
