9afd06bb82e7572f942c8e0aff759626baca01a3
Akram Boktor <akram.boktor2013@gmail.com>
2023-06-18 09:24:25 +0200

    Add Pagination for our architecture part 1

---
+++ Core/Specification/ProductWithFiltersForCountSpecification.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+
+namespace Core.Specification
+{
+    public class ProductWithFiltersForCountSpecification : BaseSpecification<Products>
+    {
+        public ProductWithFiltersForCountSpecification(ProductSpecParams productParams)
+           : base(x =>
+               (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+               (!productParams.BrandId.HasValue || x.ProductTypeId == productParams.TypeId)
+            )
+        {
+
+        }
+    }
+}
