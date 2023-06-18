ed912e8980022ddac7125b0c2804e7139f9a3b7b
Akram Boktor <akram.boktor2013@gmail.com>
2023-06-18 09:45:12 +0200

    Add Cretria search by name

--- Core/Specification/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specification/ProductsWithTypesAndBrandsSpecification.cs
@@ -6,6 +6,8 @@ namespace Core.Specification
     {
         public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams) : 
             base( x=>
+               (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) 
+               &&
                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
                (!productParams.BrandId.HasValue || x.ProductTypeId == productParams.TypeId)   
             )
