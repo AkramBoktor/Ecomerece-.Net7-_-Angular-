cf206c18afcdf03e49a1adda77d7b26a734addef
Akram Boktor <akram.boktor2013@gmail.com>
2023-07-01 23:05:18 +0200

    add product interface

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { response } from '../../node_modules/@types/express';
+import { IProduct } from './Models/products';
+import { Pagination } from './Models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +12,15 @@ import { response } from '../../node_modules/@types/express';
 export class AppComponent implements OnInit{
 
   title = 'E-Commerce';
-  products:any[] =[];
+  products:IProduct[] =[];
 
   constructor(private http:HttpClient){}
 
   ngOnInit(): void {
-     this.http.get('https://localhost:5001/api/products').subscribe({
-       next: (response:any)=> this.products = response.data, //what to do next
+     this.http.get<Pagination<IProduct[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+       next: response => this.products = response.data, //what to do next
        error: (error:any) => console.log(error), // what to do when ther is an error
-       complete:()=>{
+       complete:()=>{ 
          console.log('requested Completed');
        }
      });
