807f31ff418bcc4af827b55847e99d60ccf97b5a
Akram Boktor <akram.boktor2013@gmail.com>
2023-07-01 10:40:46 +0200

    add nav bar to project

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
