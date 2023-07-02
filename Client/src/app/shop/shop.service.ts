b4dd99f60a855de823c5fb8961d3b2773f600b19
Akram Boktor <akram.boktor2013@gmail.com>
2023-07-02 19:45:59 +0200

    add first service in shop service

--- Client/src/app/shop/shop.service.ts
+++ Client/src/app/shop/shop.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '../../../node_modules/@angular/common/http';
+import { Pagination } from '../shared/Models/pagination';
+import { IProduct } from '../shared/Models/products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+ baseUrl = 'https://localhost:5001/api/';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getProducts(){
+    return this.http.get<Pagination<IProduct[]>>(this.baseUrl + 'products');
+  }
 }
