2dc8ad34251d3c15ceee06d2a411497774a11f20
Akram-Boktor <35446384+AkramBoktor@users.noreply.github.com>
2023-07-02 20:44:24 +0200

    Client building the UI for our shop (#28)
    * add first service in shop service
    
    * add product service in shop modules
    . To create new module
      - ng g m core 
    . For skip testing file
      - ng g c shop/shop --flat --skip-tests --dry-run
    . For create a service 
      - ng g s shop/shop --skip-tests

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { response } from '../../node_modules/@types/express';
-import { IProduct } from './shared/Models/products';
-import { Pagination } from './shared/Models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,19 +6,11 @@ import { Pagination } from './shared/Models/pagination';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-
   title = 'E-Commerce';
-  products:IProduct[] =[];
 
-  constructor(private http:HttpClient){}
+  constructor(){}
 
   ngOnInit(): void {
-     this.http.get<Pagination<IProduct[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
-       next: response => this.products = response.data, //what to do next
-       error: (error:any) => console.log(error), // what to do when ther is an error
-       complete:()=>{ 
-         console.log('requested Completed');
-       }
-     });
+    
   }
 }
--- Client/src/app/shop/shop.component.ts
+++ Client/src/app/shop/shop.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../shared/Models/products';
+import { ShopService } from './shop.service';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.scss']
 })
-export class ShopComponent {
+export class ShopComponent implements OnInit{
+  products:IProduct[] =[];
+  
+  constructor(private ShopService: ShopService) {}
+  ngOnInit(): void {
 
+    this.ShopService.getProducts().subscribe({
+      next: response => this.products = response.data, //what to do next
+      error: (error:any) => console.log(error), // what to do when ther is an error
+      complete:()=>{ 
+        console.log('requested Completed');
+      }
+    });
+  }
 }
