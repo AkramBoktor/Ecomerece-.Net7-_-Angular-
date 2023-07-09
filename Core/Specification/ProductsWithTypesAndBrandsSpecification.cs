f7e79432030c30435ba9ed889b97582687509081
Akram Boktor <akram.boktor2013@gmail.com>
2023-07-09 10:39:49 +0200

    update the filter specification to solve the error of filter

--- Core/Specification/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specification/ProductsWithTypesAndBrandsSpecification.cs
@@ -9,7 +9,7 @@ namespace Core.Specification
                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) 
                &&
                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
-               (!productParams.BrandId.HasValue || x.ProductTypeId == productParams.TypeId)   
+               (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)   
             )
         {
             AddInclude(x => x.ProductType);
--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -28,7 +28,7 @@ namespace Infrastructure.Data
                 query = query.OrderByDescending(spec.OrderByDescending);
             }
 
-            query = spec.Includes.Aggregate(query, (current , include) => current.Include(include)); ;
+            query = spec.Includes.Aggregate(query, (current , include) => current.Include(include)); 
 
             return query;
         }
