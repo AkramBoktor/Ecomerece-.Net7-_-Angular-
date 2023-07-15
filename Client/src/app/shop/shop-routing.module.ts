d9cf8394f609104d7dd17d528fded503c3426313
Akram Boktor <akram.boktor2013@gmail.com>
2023-07-15 17:16:43 +0200

    lazy loading

--- Client/src/app/shop/shop-routing.module.ts
+++ Client/src/app/shop/shop-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ShopComponent } from './shop.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { Route, RouterModule } from '@angular/router';
 
-
+const routes:any =[
+  { path:'', component:ShopComponent},
+  { path:':id', component:ProductDetailsComponent},
+]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class ShopRoutingModule { }
