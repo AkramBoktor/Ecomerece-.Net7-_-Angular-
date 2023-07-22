d7292bffd742de097ad4b17863765cee8557dd2a
Akram Boktor <akram.boktor2013@gmail.com>
2023-07-22 12:32:57 +0200

    Add desgin for breadcrumb

--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ToastrModule } from 'ngx-toastr';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
 
 
@@ -11,6 +12,7 @@ import { ToastrModule } from 'ngx-toastr';
   declarations: [
     NavBarComponent,
     NotFoundComponent,
+    SectionHeaderComponent,
      
   ],
   imports: [
@@ -23,7 +25,9 @@ import { ToastrModule } from 'ngx-toastr';
     }),
   ],
   exports:[
-    NavBarComponent
+    NavBarComponent,
+    SectionHeaderComponent,
+
   ]
 })
 export class CoreModule { }
