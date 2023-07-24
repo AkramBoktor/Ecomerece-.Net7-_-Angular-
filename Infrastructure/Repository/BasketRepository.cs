44ce55e7be56f3945538715bee7c6f54e657a4c0
Akram Boktor <akram.boktor2013@gmail.com>
2023-07-24 13:20:06 +0200

    implement Repository basket

--- Infrastructure/Repository/BasketRepository.cs
+++ Infrastructure/Repository/BasketRepository.cs
@@ -1,19 +1,28 @@
 ﻿
 using Core.Entities;
 using Core.Interfaces;
+using StackExchange.Redis;
+using System.Text.Json;
 
 namespace Infrastructure.Repository
 {
     public class BasketRepository : IBasketRepository
     {
+        private readonly IDatabase _database;
+        public BasketRepository(IConnectionMultiplexer redis)
+        {
+                _database = redis.GetDatabase();
+        }
         public Task<bool> DeleteBasketAsync(string basketId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<CustomerBasket> GetBasketAsync(string basketId)
+        public async Task<CustomerBasket> GetBasketAsync(string basketId)
         {
-            throw new NotImplementedException();
+            var basketData = await _database.StringGetAsync(basketId);
+            //parse json to instance of the object
+            return basketData.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(basketData);
         }
 
         public Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
