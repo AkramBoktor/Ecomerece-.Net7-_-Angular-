b63763ff9de1b9ebaac13bad6fd87f77ca0dd57f
Akram Boktor <akram.boktor2013@gmail.com>
2023-07-29 18:55:32 +0200

    add basic for basket ( Service - component - routing )

---
+++ Client/src/app/basket/basket-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BasketComponent } from './basket.component';
+import { RouterModule } from '@angular/router';
+
+const routes:any =[
+  { path:'', component:BasketComponent}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class BasketRoutingModule { }
