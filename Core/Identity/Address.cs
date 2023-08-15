d2446c2dac7cca71c3ffea32a288362e78f9f45c
Akram Boktor <akram.boktor2013@gmail.com>
2023-08-15 12:32:15 +0200

    Add app user  class in core project

---
+++ Core/Identity/Address.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Identity
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        //one to one relationship 
+        [Required]
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
