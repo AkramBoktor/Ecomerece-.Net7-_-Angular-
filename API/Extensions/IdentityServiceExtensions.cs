11dac9b748d202737cdbdc13895b8c9d5f83384f
Akram Boktor <akram.boktor2013@gmail.com>
2023-08-15 13:06:39 +0200

    Add identity context

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Core.Data;
+using Core.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<AppIdentityDbContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("IdentityConnection"));
+            });
+
+            return services;
+        }
+    }
+}
