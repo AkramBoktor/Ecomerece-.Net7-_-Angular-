7b8f64094b350d046404b0da73d07ed2a4c00cb1
Akram Boktor <akram.boktor2013@gmail.com>
2023-08-16 09:22:59 +0200

    Add seed data and make migrarions

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Infrastructure.Identity;
+﻿using Core.Identity;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
@@ -12,6 +14,16 @@ namespace API.Extensions
                 options.UseSqlite(config.GetConnectionString("IdentityConnection"));
             });
 
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                //add identity options
+
+            })
+                .AddEntityFrameworkStores<AppIdentityDbContext>()
+                .AddSignInManager<SignInManager<AppUser>>();
+
+            services.AddAuthentication();
+            services.AddAuthorization();
             return services;
         }
     }
