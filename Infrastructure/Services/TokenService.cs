42676120e696e79aa9e1fbd79a79fe803013f7b7
Akram Boktor <akram.boktor2013@gmail.com>
2023-08-19 13:45:11 +0200

    setting up identity with token

--- Infrastructure/Services/TokenService.cs
+++ Infrastructure/Services/TokenService.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Core.Identity;
+using Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Services
 {
-    internal class TokenService
+    public class TokenService : ITokenService
     {
+        private readonly IConfiguration _configuration;
+        private readonly SymmetricSecurityKey _symmetricSecurityKey;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Token:Key"]));
+        }
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.Name,user.DisplayName)
+
+            };
+            //Add secuirty and add the signature for the token 
+            var credational = new SigningCredentials(_symmetricSecurityKey, SecurityAlgorithms.HmacSha512Signature);
+            //describe token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = credational,
+                Issuer = _configuration["Toke:Issuer"]
+            };
+
+            // handler token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
