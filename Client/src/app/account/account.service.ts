de45827ebe1eba761a135954473d3c0c3ba28d8e
Akram Boktor <akram.boktor2013@gmail.com>
2023-09-03 13:36:37 +0200

    create account service

--- Client/src/app/account/account.service.ts
+++ Client/src/app/account/account.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '../../../node_modules/@angular/common/http';
+import { IUser } from '../shared/Models/user';
+import { BehaviorSubject } from 'rxjs';
+import { Router } from '../../../node_modules/@angular/router';
+import { isMap } from 'util/types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor() { }
+  baseUrl = environment.apiUrl;
+  private currentUserSource = new BehaviorSubject<IUser | null>(null!);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private httpClient: HttpClient,private router:Router) { }
+
+  login(user:IUser){
+    return this.httpClient.post<IUser>(this.baseUrl+'account/login',user).pipe(
+      map(user=>{
+        localStorage.setItem('token',user.token);
+        this.currentUserSource.next(user);
+      })
+    )
+  }
+
+  register(user:IUser){
+    return this.httpClient.post<IUser>(this.baseUrl+'account/register',user).pipe(
+      map(user=>{
+        localStorage.setItem('token',user.token);
+        this.currentUserSource.next(user);
+      })
+    )
+  }
+
+  logout(){
+   localStorage.removeItem('token');
+   this.currentUserSource.next(null);
+   this.router.navigateByUrl('/');
+  }
+  
+  checkEmailExists(email:string){
+    return this.httpClient.get<boolean>(this.baseUrl+'account/emailexists?email='+email);
+  }
 }
