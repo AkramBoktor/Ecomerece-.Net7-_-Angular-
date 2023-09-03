4b357ac36d0f0607f9d6b6865cb04b51994ee9af
Akram Boktor <akram.boktor2013@gmail.com>
2023-09-03 13:12:59 +0200

    create user models

---
+++ Client/src/app/shared/Models/user.ts
@@ -0,0 +1,14 @@
+export interface IUser {
+    email: string;
+    displayName: string;
+    token : string;
+  }
+
+  export interface IAddress {
+      FirstName :string;
+      LastName: string;
+      Street :string;
+      City :string;
+      State:string
+      ZipCode: string
+  }
